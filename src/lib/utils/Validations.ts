2fc7872bb7191751be8f86d339ddbef5e5d1099a
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-15 15:07:23 -0300

    chore: add date_YYYY_MM_DD validation

--- src/lib/utils/Validations.ts
+++ src/lib/utils/Validations.ts
@@ -1,11 +1,34 @@
+import { format } from 'date-fns';
+import { enUS, ptBR } from 'date-fns/locale';
+
 export default class Validations {
   /**
    * verifica se o id informado está no padrão UUID
    */
   static id(uuid: string) {
-    let re = new RegExp(
+    const regex = new RegExp(
       /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/
     );
-    return re.test(uuid);
+    return regex.test(uuid);
+  }
+
+  /**
+   * verifica se a data informada está no padrão YYYY-MM-DD
+   */
+  static date_YYYY_MM_DD(date: string) {
+    const regex = new RegExp(
+      /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/
+    );
+
+    if (!regex.test(date)) return false;
+
+    try {
+      format(new Date(`${date} 12:00:00`), 'yyyy-MM-dd');
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
--- tests/unit/utils/Validations.test.ts
+++ tests/unit/utils/Validations.test.ts
@@ -1,13 +1,17 @@
 import { Validations } from 'lib/utils';
 
 describe('Validations utils tests', () => {
-  describe('id(uuid) validation', () => {
-    it('should return false if id is not an uuid', () => {
-      expect(Validations.id('id not valid')).toBe(false);
-    });
+  it('id(uuid) validation', () => {
+    expect(Validations.id('id not valid')).toBe(false);
+    expect(Validations.id('57e22f35-f7b7-46b5-96ec-14ec065014b3')).toBe(true);
+  });
 
-    it('should return true if id is an uuid', () => {
-      expect(Validations.id('57e22f35-f7b7-46b5-96ec-14ec065014b3')).toBe(true);
-    });
+  it('date YYYY_MM_DD validation', () => {
+    expect(Validations.date_YYYY_MM_DD('2019-10-01')).toBe(true);
+    expect(Validations.date_YYYY_MM_DD('2019-17-02')).toBe(false);
+    expect(Validations.date_YYYY_MM_DD('2019-14-01')).toBe(false);
+    expect(Validations.date_YYYY_MM_DD('12/05/2020')).toBe(false);
+    expect(Validations.date_YYYY_MM_DD('2019')).toBe(false);
+    expect(Validations.date_YYYY_MM_DD('2')).toBe(false);
   });
 });
