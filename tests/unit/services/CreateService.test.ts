89b0e3d14786629d7abfb6330bcad89054254abd
TiagoDiass <tiago.costadiasss@gmail.com>
2021-05-16 16:58:07 -0300

    test: add tests of CreateService service

---
+++ tests/unit/services/CreateService.test.ts
@@ -0,0 +1,92 @@
+import CreateService from 'domain/services/CreateService';
+import { IRequestCreateServiceDTO } from 'domain/services/dto';
+
+const mockedUUID = '1e16f407-1cef-427c-af39-e9c3efcbd18e';
+
+jest.mock('uuid', () => ({
+  v4: jest.fn().mockImplementation(() => mockedUUID),
+}));
+
+describe('CreateService service', () => {
+  it('should call serviceRepository.save() and return correctly', async () => {
+    const serviceRepositoryMock = {
+      list: jest.fn(),
+      save: jest.fn().mockImplementationOnce(service => ({
+        data: service,
+      })), // mocking repository.save(), it will keep returning the created service
+    };
+
+    const createService = new CreateService(serviceRepositoryMock);
+
+    const createServiceDTO: IRequestCreateServiceDTO = {
+      title: 'titulo do serviço',
+      description: 'descrição do serviço',
+    };
+
+    const response = await createService.execute(createServiceDTO);
+
+    expect(serviceRepositoryMock.save).toHaveBeenCalledWith({
+      id: mockedUUID,
+      title: createServiceDTO.title,
+      description: createServiceDTO.description,
+    });
+
+    expect(response).toEqual({
+      status: 201,
+      result: {
+        id: mockedUUID,
+        title: createServiceDTO.title,
+        description: createServiceDTO.description,
+      },
+    });
+  });
+
+  it('should return correctly if Service entity throws an exception', async () => {
+    const serviceRepositoryMock = {
+      list: jest.fn(),
+      save: jest.fn(),
+    };
+
+    const createService = new CreateService(serviceRepositoryMock);
+
+    const createServiceDTO: IRequestCreateServiceDTO = {
+      title: '', // title will be empty so Service entity will throw an exception
+    };
+
+    const response = await createService.execute(createServiceDTO);
+
+    expect(response).toEqual({
+      status: 400,
+      error: {
+        message: 'Serviço inválido',
+        errorsList: ['título do serviço deve conter pelo menos 5 caracteres'],
+      },
+    });
+  });
+
+  it('should return correctly if repository throws an exception', async () => {
+    const serviceRepositoryMock = {
+      list: jest.fn(),
+      save: jest.fn().mockImplementationOnce(() => {
+        throw new Error('Erro mockado');
+      }),
+    };
+
+    const createService = new CreateService(serviceRepositoryMock);
+
+    const createServiceDTO: IRequestCreateServiceDTO = {
+      title: 'titulo do serviço',
+      description: 'descrição do serviço',
+    };
+
+    const response = await createService.execute(createServiceDTO);
+
+    expect(response).toEqual({
+      status: 500,
+      error: {
+        message: 'Erro mockado',
+        errorsList: [],
+      },
+    });
+  });
+});
