b281dcc6fcbb2495a848ba0ac55b70ea9d4eede4
TiagoDiass <tiago.costadiasss@gmail.com>
2021-09-11 13:23:58 -0300

    feat: create a MongoConnection class to use in tests and repositories

--- jest.config.ts
+++ jest.config.ts
@@ -175,10 +175,7 @@ export default {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+  transformIgnorePatterns: ['/node_modules/(?!mongodb)', '\\.pnp\\.[^\\\\]+$'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
---
+++ src/adapters/database/connection/MongoConnection.ts
@@ -0,0 +1,66 @@
+import { MongoClient, Db } from 'mongodb';
+
+type AccessData = {
+  protocol?: string;
+  address: string;
+  port?: string;
+  user: string;
+  password: string;
+  databaseName: string;
+};
+
+/**
+ * Classe base para conexão com um banco de dados MongoDB.
+ * @class {MongoConnection}
+ */
+export default class MongoConnection {
+  private database: Db | undefined;
+  private connection: MongoClient | undefined;
+
+  constructor(private readonly accessData: AccessData) {}
+
+  public async getConnection() {
+    if (this.database) {
+      return this.database;
+    }
+
+    const protocol = this.accessData.protocol || 'mongodb://';
+    const address = this.accessData.address;
+    const port = this.accessData.port || '27017';
+    const user = this.accessData.user;
+    const password = this.accessData.password;
+    const databaseName = this.accessData.databaseName;
+
+    if (!(protocol && address && port && user && password && databaseName)) {
+      throw new Error('Faltam dados para conectar ao banco');
+    }
+
+    const url =
+      protocol +
+      user +
+      ':' +
+      password +
+      '@' +
+      address +
+      (protocol.endsWith('srv://') ? '' : ':' + port);
+
+    const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+    this.connection = mongoClient;
+
+    try {
+      await mongoClient.connect();
+      const db = mongoClient.db(databaseName);
+      this.database = db;
+      return db;
+    } catch (error: any) {
+      console.error(error);
+      return error.message;
+    }
+  }
+
+  async closeConnection() {
+    if (this.connection) {
+      await this.connection.close();
+    }
+  }
+}
