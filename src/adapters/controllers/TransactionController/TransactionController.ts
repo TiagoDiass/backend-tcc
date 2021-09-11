9c8aa23fc98a0b77ca5fc2d943435edb679e3f0f
TiagoDiass <tiago.costadiasss@gmail.com>
2021-09-11 15:54:22 -0300

    feat: add index of TransactionServices and started TransactionController

---
+++ src/adapters/controllers/TransactionController/TransactionController.ts
@@ -0,0 +1,7 @@
+import ITransactionRepository from 'domain/ports/TransactionRepository';
+import {
+  IRequestCreateServiceDTO,
+  IRequestGetTransactionByIdDTO,
+  IRequestUpdateTransactionDTO,
+  IRequestDeleteTransactionDTO,
+} from 'domain/services/dto';
---
+++ src/domain/services/TransactionServices/index.ts
@@ -0,0 +1,6 @@
+export { default as CreateTransaction } from './CreateTransaction/CreateTransaction';
+export { default as DeleteTransaction } from './DeleteTransaction/DeleteTransaction';
+export { default as GetCurrentBalance } from './GetCurrentBalance/GetCurrentBalance';
+export { default as GetTransactionById } from './GetTransactionById/GetTransactionById';
+export { default as ListTransactions } from './ListTransactions/ListTransactions';
+export { default as UpdateTransaction } from './UpdateTransaction/UpdateTransaction';
