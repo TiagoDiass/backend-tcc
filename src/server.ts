c01f0b3e6d49b807fb332b098f103fb3a0214c0d
TiagoDiass <tiago.costadiasss@gmail.com>
2021-09-29 09:05:50 -0300

    chore: add log

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,5 @@ import packageJson from '../package.json';
 const PORT = 3333;
 
 app.listen(PORT, () => {
-  console.log(
-    `[SERVER] > Server running on port ${PORT}. Version: ${packageJson.version}`
-  );
+  console.log(`[SERVER] > Server running on port ${PORT}. Version: ${packageJson.version}`);
 });
