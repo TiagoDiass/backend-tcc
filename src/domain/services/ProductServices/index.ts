c7eb878450c5f39044ae99e717f929671c79c28c
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-05 23:03:59 -0300

    feat: add index of ProductServices

---
+++ src/domain/services/ProductServices/index.ts
@@ -0,0 +1,5 @@
+export { default as ListProducts } from './ListProducts/ListProducts';
+export { default as CreateProduct } from './CreateProduct/CreateProduct';
+export { default as UpdateProduct } from './UpdateProduct/UpdateProduct';
+export { default as GetProductById } from './GetProductById/GetProductById';
+export { default as DeleteProduct } from './DeleteProduct/DeleteProduct';
