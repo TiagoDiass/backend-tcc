cce87481afc46ba7b0525470678919ec1ec613b5
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-22 23:02:18 -0300

    feat: add Medicine's index

---
+++ src/domain/services/MedicineServices/index.ts
@@ -0,0 +1,5 @@
+export { default as CreateMedicine } from './CreateMedicine/CreateMedicine';
+export { default as DeleteMedicine } from './DeleteMedicine/DeleteMedicine';
+export { default as GetMedicineById } from './GetMedicineById/GetMedicineById';
+export { default as ListMedicines } from './ListMedicines/ListMedicines';
+export { default as UpdateMedicine } from './UpdateMedicine/UpdateMedicine';
